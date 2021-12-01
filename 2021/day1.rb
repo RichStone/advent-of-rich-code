12a62bf56e9eea4bb2bd10c976c02ad6e0fd9271
Rich Steinmetz <richard.had@hotmail.de>
2021-12-01 23:38:26 +0100

    Add 2021 ruby day 1

---
+++ 2021/day1.rb
@@ -0,0 +1,10 @@
+input = open('./day1_input.txt').map(&:to_i)
+puts 'part 1:', input.each_cons(2).select { |cons| cons[0] < cons[1] }.count
+
+previous_sum = 0
+increased_cons = input.each_cons(3).select do |cons|
+  gt = cons.sum > previous_sum if previous_sum != 0
+  previous_sum = cons.sum
+  gt
+end.count
+puts 'part 2:', increased_cons
